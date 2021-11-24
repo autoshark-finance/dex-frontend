0db287dc36b709daafc52d6bd5e541e1b224289f
sharkeugene <crypto.eugene.ko@gmail.com>
2021-11-25 03:28:30 +0800

    feat: initial commit

---
+++ src/config/constants/swapWarningTokens.ts
@@ -0,0 +1,15 @@
+import { Token } from '@autoshark-finance/sdk'
+import tokens from 'config/constants/tokens'
+
+const { bondly, safemoon } = tokens
+
+interface WarningTokenList {
+  [key: string]: Token
+}
+
+const SwapWarningTokens = <WarningTokenList>{
+  safemoon,
+  bondly,
+}
+
+export default SwapWarningTokens
